1b24f776dbe2137929b7f6991f8b39bde37df9c4
Harsh Bajpai <harshbajpai520@gmail.com>
2025-03-23 15:09:55 +0530

    Updates

--- src/components/ContactData.jsx
+++
@@ -1,7 +0,0 @@
-const ContactData = {
-    Address: "151/10 N.L.C Kidwai Nagar, Kanpur",
-    PhoneNo: "+91 6378290539",
-    Email: "harsh.bajpai72@protonmail.com"
-};
-
-export default ContactData;
