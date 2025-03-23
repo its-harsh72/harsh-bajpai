40d7c5fa44fe7aab2139bcc9c353de3cd4648609
Harsh Bajpai <harshbajpai520@gmail.com>
2025-03-23 15:04:15 +0530

    first commit

---
+++ src/assets/Harsh_content.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Harsh_content = () => {
+  return (
+    <div>
+      <p>
+      I am an adept programmer with a Bachelor's degree in Technology from Laxmi Devi Institute of Engineering and Technology. I have a strong command of coding and a keen interest in frontend development. I am proficient in HTML, CSS, and React.js for creating dynamic user interfaces. On the backend, I have experience working with Node.js and Python, along with MongoDB for database management. I am passionate about building efficient and scalable web applications.
+      </p>
+       </div>
+  );
+};
+
+export default Harsh_content;
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,59 @@
+// import {logo} from "../assets/icons8-next.js-16.png"
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion"
+import Postman from '../assets/Postman.png';
+
+const iconsVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10], // The motion from 10px down to -10px up
+        transition: {
+            duration: duration,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
+    },
+});
+const Technologies = () => {
+    return (
+        <div className='pb-24'>
+            <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 1.5 }} className='my-20 text-center text-4xl'>Technologies</motion.h2>
+            <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1.5 }} className="flex flex-wrap items-center justify-center gap-4">
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(2.5)} className="p-4">
+                    <i className="fa-brands fa-js text-7xl text-yellow-500"></i>
+                </motion.div>
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(3)} className="p-4">
+                    <i className="fa-brands fa-react text-7xl text-cyan-400"></i>
+                </motion.div>
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(5)} className="p-4">
+                    <i className="fa-brands fa-node-js text-7xl text-green-500"></i>
+                </motion.div>
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(2)} className="p-4">
+                    <i className="fa-brands fa-python text-7xl text-yellow-500 transform hover:scale-110 transition-transform duration-300 shadow-lg rounded-lg"></i>
+                </motion.div>
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(7)} className="p-4">
+                    <i className="fa-brands fa-envira text-7xl text-green-500"></i>
+                </motion.div>
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(4)} className="p-4">
+                    <i className="fa-brands fa-bootstrap text-7xl text-purple-500"></i>
+                </motion.div>
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(1.8)} className="p-4">
+                    <img
+                        src={Postman}
+                        alt="Postman"
+                        className="rounded-full bg-transparent w-[80px] h-auto"
+                    />
+                </motion.div>
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(5)} className="p-4">
+                    <i className="fa-brands fa-git text-7xl text-red-500"></i>
+                </motion.div>
+                <motion.div initial="initial" animate="animate" variants={iconsVariants(6)} className="p-4">
+                    <i className="fa-brands fa-css3 text-7xl text-blue-500"></i>
+                </motion.div>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Technologies
